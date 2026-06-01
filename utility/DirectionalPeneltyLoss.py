04b760acb8cbf131b09281514c6ecf778a5b59cb
Sharman Jeurkar <sharmanjeurkar@gmail.com>
2026-06-01 16:53:41 +0530

    Added the penelty for magnitude and combined by punishing by the penelty on loss

--- utility/DirectionalPeneltyLoss.py
+++ utility/DirectionalPeneltyLoss.py
@@ -2,17 +2,20 @@ import torch
 from torch import nn
 
 
-class DirectionalLoss(nn.Module):
-    def __init__(self, weights=5):
+class DirectionaMagnitudelLoss(nn.Module):
+    def __init__(self, dir_weights=5.0, mag_scaler=1000.0):
         super().__init__()
-        self.weights = weights
+        self.dir_weights = dir_weights
+        self.mag_scaler = mag_scaler
 
     def forward(self, predictions, targets):
         a = torch.sign(predictions)
         b = torch.sign(targets)
-        loss = torch.subtract(predictions, targets)
-        loss = torch.abs(loss)
-        weighted_loss = torch.where(a == b, loss * 1, loss * 5)
-        final_loss = torch.mean(weighted_loss)
+        basic_loss = torch.subtract(predictions, targets)
+        basic_loss = torch.abs(basic_loss)
+
+        directional_penelty = torch.where(a == b, 1.0, self.dir_weights)
+        magnitude_penelty = 1.0 + (torch.abs(targets) * self.mag_scaler)
+        final_loss = torch.mean(directional_penelty * magnitude_penelty * basic_loss)
 
         return final_loss
