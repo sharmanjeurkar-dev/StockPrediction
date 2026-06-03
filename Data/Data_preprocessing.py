9ec8a2de341f9f8c719a1507c8c40d6b54359e79
Sharman Jeurkar <sharmanjeurkar@gmail.com>
2026-06-03 19:23:49 +0530

    Revrted back to single day return

--- Data/Data_preprocessing.py
+++ Data/Data_preprocessing.py
@@ -68,9 +68,7 @@ def feature_enginiering():
 
     df["ATR-Ratio"] = df["atr-s"] / df["atr-l"]
 
-    df["Target"] = (
-        df["Returns"].shift(-1) + df["Returns"].shift(-2) + df["Returns"].shift(-3)
-    )
+    df["Target"] = df["Returns"].shift(-1)
     df.dropna(inplace=True)
 
     feat_cols = [
