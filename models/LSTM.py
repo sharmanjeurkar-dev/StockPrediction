e743369bf1f5c47879db70cceec16823c1e63c47
Sharman Jeurkar <sharmanjeurkar@gmail.com>
2026-06-04 00:04:09 +0530

    Added drop out  layer in the model and a aloss parameter trasaction cost in loss

--- models/LSTM.py
+++ models/LSTM.py
@@ -8,16 +8,18 @@ class LSTM(nn.Module):
             input_size=in_size, hidden_size=hidden_units, batch_first=True
         )
 
+        self.dropout = nn.Dropout(p=0.2)
         self.fc1 = nn.Linear(in_features=hidden_units, out_features=hidden_units // 2)
         self.activation = nn.GELU()
         self.fc2 = nn.Linear(in_features=hidden_units // 2, out_features=out_features)
 
-        # adding the squashing layer which will squash my output in between -1.0 to 1.0
+        # adding the squashing layer which will gently squash my output in between -1.0 to 1.0 so that gradients dont die
         self.position_activation = nn.Softsign()
 
     def forward(self, x):
         output, _ = self.lstm_layer(x)
         x = output[:, -1, :]
+        x = self.dropout(x)
         x = self.fc1(x)
         x = self.activation(x)
         x = self.fc2(x)
--- utility/DirectionalPeneltyLoss.py
+++ utility/DirectionalPeneltyLoss.py
@@ -22,19 +22,34 @@ class DirectionaMagnitudelLoss(nn.Module):
 
 
 class SharpeRatio(nn.Module):
-    def __init__(self, risk_free_rate=0.0, holding_cost=0.0005):
+    def __init__(
+        self, risk_free_rate=0.0, holding_cost=0.0005, transaction_cost=0.00002
+    ):
         super().__init__()
         self.risk_free_rate = risk_free_rate
         self.holding_cost = holding_cost
+        self.transaction_cost = transaction_cost
 
     def forward(self, predicted_position, returns):
         pnl = predicted_position * returns
-        net_pnl = pnl - torch.abs(predicted_position) * self.holding_cost
+        position_change = torch.diff(
+            predicted_position,
+            dim=0,
+            prepend=torch.zeros(1, 1, device=predicted_position.device),
+        )
+        transaction_fee = torch.abs(position_change) * self.transaction_cost
+        holding_fee = torch.abs(predicted_position) * self.holding_cost
+        net_pnl = pnl - holding_fee - transaction_fee
 
         mean_net_pnl = torch.mean(net_pnl)
         std_net_pnl = torch.std(net_pnl)
         epsilon = 1e-6
 
-        sharpe_ratio = (mean_net_pnl - self.risk_free_rate) / (std_net_pnl + epsilon)
+        annualized_mean = 252 * mean_net_pnl
+        annualized_std = std_net_pnl * 252**0.5
+
+        sharpe_ratio = (annualized_mean - self.risk_free_rate) / (
+            annualized_std + epsilon
+        )
 
         return -sharpe_ratio
