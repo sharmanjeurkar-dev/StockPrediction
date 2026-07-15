3b1c49bb8b0b9c53f83dd84e2b70e038fc904354
Sharman Jeurkar <sharmanjeurkar@gmail.com>
2026-07-15 16:05:08 +0530

    changed the file name. Fixed a critical level bug in apply_tripple_barrier functin. also moved the complete labeling process to a different file

---
+++ Data/feature_engineering.py
@@ -0,0 +1,126 @@
+import numpy as np
+import pandas as pd
+
+
+def feature_enginiering(df: pd.DataFrame, symbol) -> pd.DataFrame:
+    df["symbol"] = symbol
+    df.sort_index(inplace=True)
+    df = df.loc[~df.index.duplicated(keep="first")].copy()
+
+    # Calculating the %returns on close price if the share was brought
+    df["Returns"] = df["Close"].pct_change()
+
+    # RSI score
+    df["Change"] = df["Close"].diff()
+    df["Gain"] = np.where(df["Change"] > 0, df["Change"], 0)
+    df["Loss"] = np.where(df["Change"] < 0, abs(df["Change"]), 0)
+
+    df["Avg_Gain"] = df["Gain"].rolling(window=14).mean()
+    df["Avg_Loss"] = df["Loss"].rolling(window=14).mean()
+
+    df["Rs"] = df["Avg_Gain"] / df["Avg_Loss"]
+    df["RSI-close-score"] = 100 - 100 / (1 + df["Rs"])
+
+    # EMA Sscores for different windows
+    df["EMA-Today-close-26D"] = df["Close"].ewm(span=26, adjust=False).mean()
+    df["EMA-Today-close-12D"] = df["Close"].ewm(span=12, adjust=False).mean()
+
+    # MACD Scores
+    df["MACD-Line"] = df["EMA-Today-close-12D"] - df["EMA-Today-close-26D"]
+    df["Single-Line"] = df["MACD-Line"].ewm(span=9, adjust=False).mean()
+    df["MACD-Histogram"] = df["MACD-Line"] - df["Single-Line"]
+    df.drop(columns=["EMA-Today-close-26D", "EMA-Today-close-12D"], inplace=True)
+
+    # Bollinger Bandwidth
+    df["Middle-Band"] = df["Close"].rolling(window=20).mean()
+    df["Upper-Band"] = df["Middle-Band"] + 2 * df["Close"].rolling(window=20).std()
+    df["Lower-Band"] = df["Middle-Band"] - 2 * df["Close"].rolling(window=20).std()
+
+    df["Bollinger-Bandwidth"] = (df["Upper-Band"] - df["Lower-Band"]) / df[
+        "Middle-Band"
+    ]
+    df["%-Band"] = (df["Close"] - df["Lower-Band"]) / (
+        df["Upper-Band"] - df["Lower-Band"]
+    )
+    df["Intraday_Spread"] = (df["High"] - df["Low"]) / df["Close"]
+    # df["Volume_Price_Velocity"] = df["Returns"] * (
+    #     df["Volume"] / df["Volume"].rolling(20).mean()
+    # )
+
+    # On-Balance Volume (OBV)
+    condition = [(df["Change"] > 0), (df["Change"] < 0)]
+    choices = [1, -1]
+    df["obv-dir"] = np.select(condition, choices, default=0)
+    df["OBV"] = (df["Volume"] * df["obv-dir"]).cumsum()
+    df["OBV-ROC"] = df["OBV"].pct_change(10).replace([np.inf, -np.inf], 0).fillna(0)
+    df["Volume-Rate-of-Change"] = (df["Volume"].pct_change(periods=10)).replace(
+        [np.inf, -np.inf], 0
+    ).fillna(0) * 100
+
+    # ATR Compressoin Ratio
+    df["high_low"] = df["High"] - df["Low"]
+    df["high_prev_close"] = (df["High"] - df["Close"].shift(1)).abs()
+    df["low_prev_close"] = (df["Low"] - df["Close"].shift(1)).abs()
+
+    df["true_range"] = df[["high_low", "high_prev_close", "low_prev_close"]].max(axis=1)
+    df["atr-s"] = df["true_range"].rolling(window=14).mean()
+    df["atr-l"] = df["true_range"].rolling(window=50).mean()
+
+    df["ATR-Ratio"] = df["atr-s"] / df["atr-l"]
+    df.drop(columns=["high_low", "high_prev_close", "low_prev_close"], inplace=True)
+
+    minutes_elapsed = (df.index.hour * 60 + df.index.minute) - 555
+    time_fraction = np.clip(minutes_elapsed / 375.0, 0.0, 1.0)
+    df["Time_Sin"] = np.sin(time_fraction * 2 * np.pi)
+    df["Time_Cos"] = np.cos(time_fraction * 2 * np.pi)
+    df.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+    df["Daily_Vol"] = df["Close"].pct_change().rolling(window=25).std()
+
+    df["MA-20"] = df["Close"].rolling(window=20).mean()
+    df["MA-50"] = df["Close"].rolling(window=50).mean()
+    df["MA-Cross"] = (df["MA-20"] - df["MA-50"]) / df["MA-50"]
+
+    df.dropna(inplace=True)
+
+    return df
+
+
+def concat_df(dfs: list[pd.DataFrame]) -> pd.DataFrame:
+    processed = []
+
+    for df in dfs:
+        symbol = df["symbol"].iloc[0]
+        df_processed = feature_enginiering(df, symbol)
+        df_processed["symbol"] = symbol
+        processed.append(df_processed)
+
+    final_df = pd.concat(processed, ignore_index=False)
+    return final_df
+
+
+def walk_forward_slices(
+    df: pd.DataFrame,
+    date_col: str = "Datetime",
+    symbol_col: str = "symbol",
+    embargo_candles: int = 8,
+) -> list:
+    df = df.copy()
+    df[date_col] = pd.to_datetime(df[date_col])
+    years = sorted(df[date_col].dt.year.unique())
+    append_df = []
+
+    for i in range(1, len(years)):
+        train_years = years[:i]
+        train_df = df[df[date_col].dt.year.isin(train_years)].copy()
+
+        if embargo_candles > 0:
+            train_df = train_df.sort_values([symbol_col, date_col])
+            row_rank_desc = train_df.groupby(symbol_col).cumcount(ascending=False)
+            train_df = train_df[row_rank_desc >= embargo_candles]
+
+        test_year = years[i]
+        test_df = df[df[date_col].dt.year == test_year].copy()
+        append_df.append((train_df, test_df))
+
+    return append_df
